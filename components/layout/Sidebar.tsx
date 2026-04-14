d64d63214fe2970aef7d529c08bdd3373cf2fd79
Matt Durkin <m.durkin92@gmail.com>
2026-04-14 11:54:03 +0100

    fix capitalisation of 'FAQ' in sidebar menu items

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -142,7 +142,7 @@ export function Sidebar() {
       if (typeof item === 'string') {
         const href = `/${item}`
         const isActive = pathname === href
-        const ACRONYMS = new Set(['ai', 'api', 'dao', 'eth', 'evm', 'nft', 'rpc', 'uri', 'url'])
+        const ACRONYMS = new Set(['ai', 'api', 'dao', 'eth', 'evm', 'faq', 'nft', 'rpc', 'uri', 'url'])
         const title = item.split('/').pop()?.split('-').map(w =>
           ACRONYMS.has(w) ? w.toUpperCase() : w.charAt(0).toUpperCase() + w.slice(1)
         ).join(' ') || item
